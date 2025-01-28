89e0500ec60c69cde5e4d10d7d7e2c088cffe974
Ziyad Derfoufi <zderfouf@e1r10p3.1337.ma>
2025-01-28 17:09:23 +0100

    [mkdir]

---
+++ cpp02/ex03/Fixed.cpp
@@ -0,0 +1,167 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : N(0) {
+	// std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value) : N(value << Fractional_b) {
+	// std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float value) : N((int) roundf(value * (1 << Fractional_b))) {
+	// std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) : N(other.N) {
+	// std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	// std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits( void ) const
+{
+	// std::cout << "getRawBits member function called" << std::endl;
+	return this->N;
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	// std::cout << "setRawBits member function called" << std::endl;
+	this->N = raw;
+}
+
+int		Fixed::toInt( void ) const
+{
+	return ((int) roundf ((float)this->N / (1 << this->Fractional_b)));
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return((float)this->N / (1 << this->Fractional_b));
+}
+
+Fixed	&Fixed::operator=(const Fixed &other)
+	// we pass by reference to not create a new copy of the Obj. (to not call CP CONS)
+	// const to not change the "other" object, for safety.
+/*
+	return reference in case of chain assignements , a = b = c.
+	it will keep returning a copy but now the Object itself.
+	the CP CONS will be called twice bec it will create a copy to return it.
+*/
+
+{
+	// std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->N = other.getRawBits();
+	return (*this);
+	// return *this because we need to return reference to the object pointed by this
+}
+
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return (out);
+}
+
+Fixed	Fixed::min(Fixed &left, Fixed &right)
+{
+	if (left > right)
+		return ( right );
+	return ( left );
+}
+
+Fixed	Fixed::min( const Fixed &left, const Fixed &right)
+{
+	if (left > right)
+		return ( right);
+	return (left);
+}
+
+Fixed	Fixed::max( Fixed &left, Fixed &right)
+{
+	if (left >= right)
+		return ( left );
+	return ( right );
+}
+
+Fixed	Fixed::max( const Fixed &left, const Fixed &right)
+{
+	if (left >= right)
+		return (left);
+	return (right);
+}
+
+Fixed	Fixed::operator+( const Fixed &other ) const
+{
+	return (Fixed(other.toFloat() + this->toFloat()));
+}
+
+Fixed	Fixed::operator-( const Fixed &other ) const
+{
+	return (Fixed(other.toFloat() - this->toFloat()));
+}
+
+Fixed	Fixed::operator*( const Fixed &other ) const
+{
+	return (Fixed(other.toFloat() * this->toFloat()));
+}
+
+Fixed	Fixed::operator/( const Fixed &other ) const
+{
+	return (Fixed(other.toFloat() / this->toFloat()));
+}
+
+bool	Fixed::operator>( const Fixed &other ) const
+{
+	return (this->toFloat() > other.toFloat());
+}
+bool	Fixed::operator<( const Fixed &other ) const
+{
+	return (this->toFloat() < other.toFloat());
+}
+bool	Fixed::operator>=( const Fixed &other ) const
+{
+	return (this->toFloat() >= other.toFloat());
+}
+
+bool	Fixed::operator<=( const Fixed &other ) const
+{
+	return (this->toFloat() <= other.toFloat());
+}
+
+bool	Fixed::operator==( const Fixed &other ) const
+{
+	return (this->toFloat() == other.toFloat());
+}
+
+bool	Fixed::operator!=( const Fixed &other ) const
+{
+	return (this->toFloat() != other.toFloat());
+}
+
+Fixed	&Fixed::operator++( void )
+{
+	return (this->N++, *this);
+}
+
+Fixed	&Fixed::operator--( void )
+{
+	return (this->N--, *this);
+}
+
+Fixed	Fixed::operator++( int n )
+{
+	Fixed	tmp = *this;
+	this->N++;
+	return (tmp);
+}
+
+Fixed	Fixed::operator--( int n )
+{
+	Fixed	tmp = *this;
+	this->N--;
+	return (tmp);
+}
---
+++ cpp02/ex03/Fixed.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int				N;
+	static const int Fractional_b = 8;
+public:
+	Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed(const Fixed &other);
+	~Fixed();
+
+
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+
+	int		toInt( void ) const;
+	float	toFloat( void ) const;
+
+	Fixed	&operator=(const Fixed &other);
+	Fixed	operator+( const Fixed &other ) const;
+	Fixed	operator-( const Fixed &other ) const; 
+	Fixed	operator*( const Fixed &other ) const;
+	Fixed	operator/( const Fixed &other ) const;
+	Fixed	&operator++( void );
+	Fixed	&operator--( void );
+	Fixed	operator++( int n );
+	Fixed	operator--( int n );
+
+	bool	operator>( const Fixed &other ) const;
+	bool	operator<( const Fixed &other ) const;
+	bool	operator>=( const Fixed &other ) const;
+	bool	operator<=( const Fixed &other ) const;
+	bool	operator==( const Fixed &other ) const;
+	bool	operator!=( const Fixed &other ) const;
+
+
+	static Fixed	min( Fixed &left, Fixed &right);
+	static Fixed	min( const Fixed &left, const Fixed &right);
+
+	static Fixed	max( Fixed &left, Fixed &right);
+	static Fixed	max( const Fixed &left, const Fixed &right);
+};
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed );
