2781301789c4225ece082d898556c28a404b08e2
Ziyad Derfoufi <zderfouf@e2r3p4.1337.ma>
2025-02-08 10:14:19 +0100

    [inheritance understanding]

--- cpp03/z.cpp
+++ cpp03/z.cpp
@@ -7,7 +7,7 @@ public:
 	int a = 42;
 	A() { cout << "Constructing A" << endl; }
 
-protected:
+// protected:
 	void Base_fun() { cout << "I am in class A" << endl; }
 };
 
@@ -16,7 +16,7 @@ public:
 	char b = 'b';
 	B() { cout << "Constructing B" << endl; }
 
-protected:
+// protected:
 	void Derived_fun() { cout << "I am in class B" << endl; }
 };
 
@@ -25,7 +25,7 @@ public:
 	char c = 'c';
 	C() { cout << "Constructing C" << endl; }
 
-protected:
+// protected:
 	void Derived_fun() { cout << "I am in class C" << endl; }
 };
 
@@ -34,7 +34,7 @@ public:
 	char d = 'd';
 	D() { cout << "Constructing D" << endl; }
 
-protected:
+// protected:
 	void Derived_fun() { cout << "I am in class D" << endl; }
 };
 
@@ -53,22 +53,33 @@ int main()
 {
 	X _xoxo;
 
-	/**
+/**
 	 * @brief printing the value of a (variable in the Base class).
-	 * 
-	 */
+*/
 	cout << _xoxo.a << endl; // i can do this
 	cout << _xoxo.A::a << endl; // and i also can do this
 
-	/**
+/**
 	 * @brief printing the values of b, c, and d.
-	 * 
-	 */
+*/
 
 	cout << _xoxo.b << endl;
 	cout << _xoxo.c << endl;
 	cout << _xoxo.d << endl;
 	cout << "********************" << endl;
 
-	_xoxo.Derived_fun();
+/**
+	 * @brief this will cause Compile-time error (ambiguous)
+	 * i need to specify which class method to use using ::B or ::C or ::D
+			_xoxo.Derived_fun();
+*/
+	_xoxo.B::Derived_fun();
+
+/**
+	 * @brief this will cause Compile-time error (‘void A::Base_fun()’ is protected within this context)
+			_xoxo.Base_fun();
+	 * i need to declare function as public or access the function via the X as a method.
+*/
+	_xoxo.Base_fun(); // calling Base_fun() from X
+	_xoxo.B::Base_fun(); // calling Base_fun() from B
 }
