746a3f564c6d13b81c5952ffbdd4037f86aeb0e1
Ziad Derfoufi <ziadderfoufi@Ziads-MacBook-Pro.local>
2025-02-20 06:05:41 +0100

    [new main.c]

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -9,19 +9,50 @@
 
 void f(){system("leaks ex01");}
 
-int main()
-{
-	atexit(f);
-    Dog	*dogBrain = new Dog();
+int main() {
 
-	std::cout << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
 
-	dogBrain->makeSound();
+    Dog* malinois = new Dog();
+    malinois->setIdea("Chase the ball!");
 
-	std::string allo = "eat";
-	dogBrain->setIdea(allo);
-	std::cout << dogBrain->getIdea(0) << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
 
-	std::cout << std::endl;
-	delete dogBrain;
+    Dog* malinois2 = new Dog(*malinois);
+
+    std::cout << "-------------------------------------" << std::endl;
+
+    std::cout << "malinois Idea : " << malinois->getIdea(0) << std::endl;
+    std::cout << "malinois2 Idea : " << malinois2->getIdea(0) << std::endl;
+
+    std::cout << "-------------------------------------" << std::endl;
+
+    malinois->setIdea("Sleep on the couch.");
+
+    std::cout << "malinois New Idea : " << malinois->getIdea(0) << std::endl;
+    std::cout << "malinois2 New Idea : " << malinois2->getIdea(0) << std::endl;
+
+    std::cout << "-------------------------------------" << std::endl;
+
+    delete malinois;
+    delete malinois2;
+
+    std::cout << "-------------------------------------" << std::endl;
+
+    Animal* animals[2];
+
+    animals[0] = new Dog();
+
+    std::cout << "-------------------------------------" << std::endl;
+
+    animals[1] = new Cat();
+
+    std::cout << "-------------------------------------" << std::endl;
+
+    for (int k = 0; k < 2; k++)
+        delete animals[k];
+
+    std::cout << "-------------------------------------" << std::endl;
+
+    return 0;
 }
