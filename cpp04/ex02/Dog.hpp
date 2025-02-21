94d6be996b926900735625a0f970facf85b38648
Ziyad Derfoufi <zderfouf@e2r3p4.1337.ma>
2025-02-21 08:38:15 +0100

    [added ex02] 	difference is the pure virtual function

---
+++ cpp04/ex02/Dog.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Dog : public Animal {
+private:
+	Brain *B_ptr;
+public:
+	Dog();
+	Dog(const Dog &other);
+	Dog	&operator=(const Dog &other);
+	virtual	~Dog();
+
+	const std::string	getIdea(size_t index) const;
+
+	void	makeSound() const;
+};
---
+++ cpp04/ex02/main.cpp
@@ -0,0 +1,46 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main() {
+
+/**
+ * @brief subject's test
+ * 
+ */
+
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << std::endl;
+
+	delete j; //should not create a leak
+	delete i;
+
+	std::cout << "\n*********************\n" << std::endl;
+
+/**
+ * @brief my test
+ * 
+ */
+
+    Animal* _animal[2];
+	_animal[0] = new Dog();
+	_animal[1] = new Cat();
+
+    std::cout << std::endl;
+
+	std::cout << _animal[0]->getType() << std::endl;
+	std::cout << _animal[1]->getType() << std::endl;
+
+    std::cout << std::endl;
+
+	std::cout << ((Dog *)_animal[0])->getIdea(95) << std::endl;
+	std::cout << ((Cat *)_animal[1])->getIdea(95) << std::endl;
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < 2; i++)
+        delete _animal[i];
+    return 0;
+}
