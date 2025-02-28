74ea527441546ee0344fcdb75d173818e6515208
Ziad Derfoufi <ziadderfoufi@Ziads-MacBook-Pro.local>
2025-02-28 15:35:13 +0000

    [working on Character.cpp file -- not done yet!]

--- Module_04/ex03/Character.cpp
+++ Module_04/ex03/Character.cpp
@@ -37,13 +37,39 @@ std::string const&  Character::getName() const {
 }
 
 void	Character::equip(AMateria* m) {
-
+	if (index < 0 || index > 49)
+	{
+		std::cout << "Invalid floor index!" << std::endl;
+		return;
+	}
 }
 
-void	Character::unequip(int idx) {
-
+void	Character::unequip(int index) {
+	if (index < 0 || index > 3)
+	{
+		std::cout << "Invalid inventory index!" << std::endl;
+		return ;
+	}
+	if (!this->inventory[index])
+	{
+		std::cout << "Inventory slot already empty!" << std::endl;
+		return ;
+	}
 }
 
-void	Character::use(int idx, ICharacter& target) {
-
+void	Character::use(int index, ICharacter& target) {
+	if (index < 0 || index > 3)
+	{
+		std::cout << "Invalid inventory index!" << std::endl;
+		return ;
+	}
+	if (!this->inventory[index])
+	{
+		std::cout << "Slot " << index << " is empty!" << std::endl;
+		return;
+	}
+	std::cout << name << ": ";
+	this->inventory[index]->use(target);
+	delete this->inventory[index];
+	this->inventory[index] = NULL;
 }
--- Module_04/ex03/ICharacter.hpp
+++ Module_04/ex03/ICharacter.hpp
@@ -12,5 +12,5 @@ public:
 	virtual std::string const & getName() const = 0;
 	virtual void equip(AMateria* m) = 0;
 	virtual void unequip(int idx) = 0;
-	virtual void use(int idx, ICharacter& target) = 0;
+	virtual void use(int index, ICharacter& target) = 0;
 };
