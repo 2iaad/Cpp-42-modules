ca28419cd164866a0f8d8fec8fc3a9105deea7d8
Ziad Derfoufi <ziadderfoufi@MacBookPro.home>
2025-03-23 20:05:42 +0000

    ./ex00 *Character as input* working perfectly

--- Module_06/ex00/ScalarConverter.cpp
+++ Module_06/ex00/ScalarConverter.cpp
@@ -23,23 +23,10 @@ ScalarConverter::~ScalarConverter()
 {
 	std::cout << "ScalarConverter Destructor called" << std::endl;
 }
-
 /*	|#-----------------------------------------------------#|
 	|#						Identifyer 					   #|
 	|#-----------------------------------------------------#|
 */
-
-void	identifyType(std::string	&arg, t_data	*s)
-{
-	if (CheckChar(arg))			s->type = Character;
-	else if (CheckInt(arg))		s->type = Integer;
-	else if (CheckFloat(arg))	s->type = Float;
-	else if (CheckDouble(arg))	s->type = Double;
-	else if (CheckLiteral(arg))	s->type = Literal;
-	else
-		std::cout << "Walou !" << std::endl;
-}
-
 bool	CheckChar(std::string	&arg)
 {
     return arg.size() == 1 && !std::isdigit(arg[0]);
@@ -116,17 +103,20 @@ bool	CheckLiteral(std::string	&arg)
 	return false ;
 }
 
+void	identifyType(std::string	&arg, t_data	*s)
+{
+	if (CheckChar(arg))			s->type = Character;
+	else if (CheckInt(arg))		s->type = Integer;
+	else if (CheckFloat(arg))	s->type = Float;
+	else if (CheckDouble(arg))	s->type = Double;
+	else if (CheckLiteral(arg))	s->type = Literal;
+	else
+		std::cout << "Walou !" << std::endl;
+}
 /*	|#-----------------------------------------------------#|
 	|#						Printers 					   #|
 	|#-----------------------------------------------------#|
 */
-
-	// errno = 0;
-	// double result = std::strtod(str.c_str(), NULL);
-
-	// if (errno == ERANGE || ((Scalar.type != 2 && Scalar.type != 5) && *rest != '\0'))
-	// 	std::cerr << "HERE!\n";
-
 void	printLiteral(t_data s)
 {
 	std::cout << "char: impossible!" << std::endl;
@@ -137,7 +127,6 @@ void	printLiteral(t_data s)
 
 void	printNumber(t_data s)
 {
-
 	if (std::isprint(s.Char))
 		std::cout << "char: '" << s.Char << "'" << std::endl;
 	else
@@ -147,37 +136,45 @@ void	printNumber(t_data s)
 	std::cout << "double: " << s.Double << std::endl;
 }
 
-void	printer(std::string	&str, t_data *s)
+void	printer(std::string	&str, t_data s)
 {
-	s->Double = std::strtod(str.c_str(), NULL);
-
-	if (s->Double < INT_MIN || s->Double > INT_MAX)
-		s->type = Literal;
-	s->Float = static_cast< float > (s->Double);
-	s->Integer = static_cast< int > (s->Double);
-	s->Char = static_cast< char > (s->Double);
-
-	switch (s->type)
+	if (s.type == Character) // ila kan *char* -> makhasch ndwzo l strtod()
 	{
-		case 0 :	return printNumber(*s);
-		case 1 :	return printNumber(*s);
-		case 2 :	return printNumber(*s);
-		case 3 :	return printNumber(*s);
-		case 4 :	return printLiteral(*s);
+		s.Char = str[0];
+		s.Integer = static_cast< int >(s.Char);
+		s.Float = static_cast< float >(s.Integer);
+		s.Double = static_cast< int >(s.Integer);
+	}
+	else
+	{
+		s.Double = std::strtod(str.c_str(), NULL);
+
+		if (s.Double < INT_MIN || s.Double > INT_MAX)
+			s.type = Literal;
+		s.Float = static_cast< float > (s.Double);
+		s.Integer = static_cast< int > (s.Double);
+		s.Char = static_cast< char > (s.Double);
+	}
+
+	switch (s.type)
+	{
+		case 0 :	return printNumber(s);
+		case 1 :	return printNumber(s);
+		case 2 :	return printNumber(s);
+		case 3 :	return printNumber(s);
+		case 4 :	return printLiteral(s);
 		default :
 			return (void) (std::cout << "Unknown type !" << std::endl);
 	}
 }
-
 /*	|#-----------------------------------------------------#|
 	|#						Converter 					   #|
 	|#-----------------------------------------------------#|
 */
-
 void	ScalarConverter::convert(std::string	toConvert)
 {
 	t_data	s;
 
 	identifyType(toConvert, &s);
-	printer(toConvert, &s);
+	printer(toConvert, s);
 }
