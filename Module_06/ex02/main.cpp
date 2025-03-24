c0cd05547ba779175d9cad98ad8c4bd9bb4d5152
Ziad Derfoufi <ziadderfoufi@MacBookPro.home>
2025-03-24 04:13:58 +0000

    [ex02 done Perfectly -- smallest ex02 fkawkab]

--- Module_06/ex02/main.cpp
+++ Module_06/ex02/main.cpp
@@ -5,31 +5,58 @@
 
 Base	*generate(void)
 {
-	switch (rand() % 3)
+	switch (time(NULL) % 3)
 	{
 		case 0 : return std::cout << "A Created !" << std::endl, new A();
 		case 1 : return std::cout << "B Created !" << std::endl, new B();
 		case 2 : return std::cout << "C Created !" << std::endl, new C();
-		default :
-			std::cout << "Couldn't Generate !" << std::endl;
-			return NULL ;
-
 	}
+	std::cout << "Couldn't Generate !" << std::endl;
+	return NULL ;
 }
 
 void	identify(Base	*p)
 {
+	if (dynamic_cast<A *>(p))		std::cout << "Pointer identification -> A" << std::endl;
+	else if (dynamic_cast<B *>(p))	std::cout << "Pointer identification -> B" << std::endl;
+	else if (dynamic_cast<C *>(p))	std::cout << "Pointer identification -> C" << std::endl;
 }
 
 void	identify(Base	&p)
 {
+    try {
+        (void)dynamic_cast< A& >(p);
+        std::cout << "Reference identification -> A" << std::endl;
+    } catch(const std::exception& e) { }
+
+    try {
+        (void)dynamic_cast< B& >(p);
+        std::cout << "Reference identification -> B" << std::endl;
+    } catch( const std::exception& e ) { }
+
+    try {
+        (void)dynamic_cast< C& >(p);
+        std::cout << "Reference identification -> C" << std::endl;
+    } catch( const std::exception& e ) { }
 }
 
 int main()
 {
-	Base	*ptr;
+	{
+		Base	*ptr;
 
-	ptr = generate();
+		ptr = generate();
+		identify(ptr);
+		identify(*ptr);
 
-	delete ptr;
+		delete ptr;
+	}
+	std::cout << std::endl;
+	{
+		A	obj;
+		(Base)obj;
+
+		identify(&obj);
+		identify(obj);
+	}
 }
